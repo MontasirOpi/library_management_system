1a190e68f3b7687c555f6e9f4bf3ff57f2748b1d
S. M. Tahmidul Islam <136302787+tahmidulislamfahim@users.noreply.github.com>
2025-04-14 17:35:05 +0600

    model

--- lib/features/homePage/ui/screens/homepage.dart
+++ lib/features/homePage/ui/screens/homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:library_management_system/app/app_color.dart';
-import 'package:library_management_system/features/homePage/data/book.dart';
 import 'package:library_management_system/features/homePage/data/book_list.dart';
+import 'package:library_management_system/features/homePage/model/search.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -42,88 +42,7 @@ class HomePage extends StatelessWidget {
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(16),
               ),
-              child: TextField(
-                onSubmitted: (query) {
-                  final foundBooks =
-                      BookList.books
-                          .where(
-                            (book) => book.title.toLowerCase().contains(
-                              query.toLowerCase(),
-                            ),
-                          )
-                          .toList();
-                  if (foundBooks.isNotEmpty) {
-                    showDialog(
-                      context: context,
-                      builder:
-                          (context) => AlertDialog(
-                            title: const Text("Search Results"),
-                            content: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children:
-                                  foundBooks
-                                      .map(
-                                        (book) => Card(
-                                          shape: RoundedRectangleBorder(
-                                            borderRadius: BorderRadius.circular(
-                                              16,
-                                            ),
-                                          ),
-                                          elevation: 4,
-                                          child: ListTile(
-                                            leading: ClipRRect(
-                                              borderRadius:
-                                                  BorderRadius.circular(8),
-                                              child: Image.asset(
-                                                book.image,
-                                                height: 50,
-                                                width: 50,
-                                                fit: BoxFit.cover,
-                                              ),
-                                            ),
-                                            title: Text(book.title),
-                                            trailing: IconButton(
-                                              icon: const Icon(
-                                                Icons.arrow_forward,
-                                              ),
-                                              onPressed: () {},
-                                            ),
-                                          ),
-                                        ),
-                                      )
-                                      .toList(),
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: const Text("Close"),
-                              ),
-                            ],
-                          ),
-                    );
-                  } else {
-                    showDialog(
-                      context: context,
-                      builder:
-                          (context) => AlertDialog(
-                            title: const Text("No Results"),
-                            content: const Text("No books found."),
-                            actions: [
-                              TextButton(
-                                onPressed: () => Navigator.pop(context),
-                                child: const Text("Close"),
-                              ),
-                            ],
-                          ),
-                    );
-                  }
-                },
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.search, color: AppColors.themeColor),
-                  border: InputBorder.none,
-                  hintText: "Search",
-                ),
-              ),
+              child: Search(),
             ),
             const SizedBox(height: 20),
             // Sections
