80cf0ab2078c5d3a1a3a55ee3e8cf6cda068e169
S. M. Tahmidul Islam <136302787+tahmidulislamfahim@users.noreply.github.com>
2025-04-16 03:47:21 +0600

    Issued books page

---
+++ lib/features/common/widgets/action_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ActionButton extends StatelessWidget {
+  const ActionButton({super.key, required this.text, required this.onPressed});
+
+  final String text;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue.shade900,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      ),
+      child: Text(text, style: const TextStyle(color: Colors.white)),
+    );
+  }
+}
--- lib/features/homePage/widgets/app_drawer.dart
+++ lib/features/homePage/widgets/app_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:library_management_system/features/homePage/ui/screens/issued_books.dart';
 import 'package:library_management_system/features/my%20profile/my_profile.dart';
+import 'package:library_management_system/app/app_color.dart';
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({super.key});
@@ -7,61 +9,52 @@ class AppDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Drawer(
       backgroundColor: Colors.white,
-      child: Column(
-        children: [
-          const DrawerHeader(
-            decoration: BoxDecoration(color: Colors.white),
-            child: Center(
-              child: Text(
-                'Library Management System',
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.deepPurple,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.white),
+              child: Center(
+                child: Text(
+                  'Library Management System',
+                  style: TextStyle(
+                    color: AppColors.themeColor,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
             ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.person, color: Colors.indigo),
-            title: const Text('My Profile'),
-            onTap: () {
-              // Navigate to My Profile Page
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const MyProfile()),
-              );
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.menu_book, color: Colors.indigo),
-            title: const Text('Issued Books'),
-            onTap: () {
-              // Navigate to Issued Books Page
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.notifications, color: Colors.indigo),
-            title: const Text('Notifications'),
-            onTap: () {
-              // Navigate to Notifications
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.help, color: Colors.indigo),
-            title: const Text('Help'),
-            onTap: () {
-              // Navigate to Help page
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.settings, color: Colors.indigo),
-            title: const Text('Settings'),
-            onTap: () {
-              // Navigate to Settings page
-            },
-          ),
-        ],
+            ListTile(
+              leading: const Icon(Icons.person, color: Colors.indigo),
+              title: const Text('My Profile'),
+              onTap: () {
+                // Navigate to My Profile Page
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const MyProfile()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.menu_book, color: Colors.indigo),
+              title: const Text('Issued Books'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const IssuedBooks()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.notifications, color: Colors.indigo),
+              title: const Text('Notifications'),
+              onTap: () {
+                // Navigate to Notifications
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
