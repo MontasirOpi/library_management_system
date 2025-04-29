4041d973b6fa614e4f40232bf70dbadb25cba894
S. M. Tahmidul Islam <136302787+tahmidulislamfahim@users.noreply.github.com>
2025-04-29 17:56:09 +0600

    color

--- lib/app/app_color.dart
+++ lib/app/app_color.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // Other color definitions
-
-  static const Color themeColor = Color(
-    0xFF07ADAE,
-  ); // Replace with your desired color
+  static const Color themeColor = Color(0xFF07ADAE);
 }
